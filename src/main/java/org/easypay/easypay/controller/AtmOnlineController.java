830c5467582f114035a3ed9eb3aa2ee41a6505c7
simo <simone.picco@edu.unito.it>
2020-03-17 21:51:19 +0100

    angular controller pathed

--- src/main/java/org/easypay/easypay/controller/AtmOnlineController.java
+++ src/main/java/org/easypay/easypay/controller/AtmOnlineController.java
@@ -5,22 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Gestisce i redirect dell' applicazione angular per farla funzionare correttamente, per permettere la gestione
- * del routing internamente
+ * Gestisce i redirect dell' applicazione angular per farla funzionare
+ * correttamente, per permettere la gestione del routing internamente
  */
 @Controller
-@RequestMapping(value = "/easypay-online")
-public class EasyPayController {
+@RequestMapping(value = "/atm")
+public class AtmOnlineController {
 
     @GetMapping(value = {"/", ""})
     public String redirectHome() {
         // Forward to home page so that angular routing is preserved.
-        return "forward:/easypay-online/index.html";
+        return "forward:/atm/index.html";
     }
 
     @GetMapping(value = "/**/{[path:[^\\.]*}")
     public String redirect() {
         // Forward to home page so that angular routing is preserved.
-        return "forward:/easypay-online/index.html";
+        return "forward:/atm/index.html";
     }
 }
