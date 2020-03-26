31b891ec03ea56e7eac54b4ebfd8b0e3b02e29e3
Giuseppe Mammolo <gmammolo@gmail.com>
2020-03-26 18:45:48 +0100

    [EP-ATM] fix login template

--- src/main/angular/easypay-atm/src/app/features/login-page/login-page.component.ts
+++ src/main/angular/easypay-atm/src/app/features/login-page/login-page.component.ts
@@ -7,13 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  public breakpoint: number;
+  public  maxCol = 6;
+  public  minCol = 2;
+
+  private deviceSize = 850;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.breakpoint = (window.innerWidth <= this.deviceSize) ? this.minCol : this.maxCol;
   }
 
   clickInfo() {
     console.warn('not yet implement');
   }
+
+  randomColor(): string {
+    // return 'white';
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+
+onResize(event) {
+  this.breakpoint = (event.target.innerWidth <= this.deviceSize) ? this.minCol : this.maxCol;
+}
 
 }
